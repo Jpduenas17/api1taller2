449378e9c01ca50f09aa900444ee175d6a41b4e9
Jpduenas17 <danpao45@gmail.com>
2022-08-18 22:02:12 -0500

    Implementar logs

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import requests
+import logging.config
 
 from typing import Union
 
 from fastapi import FastAPI
 from pydantic import BaseModel
 from prometheus_fastapi_instrumentator import Instrumentator 
+
+
+logging.config.fileConfig('logging.conf', disable_existing_loggers=False)
+logger = logging.getLogger(__name__)  # the __name__ resolve to "main" since we are at the root of the project. 
+                                      # This will get the root logger since no logger in the configuration has this name.
+
 
 app = FastAPI()
 
