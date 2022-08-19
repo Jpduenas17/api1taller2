23606b26a218d03b9a2ca69076e0dfd34518c2b6
Jpduenas17 <danpao45@gmail.com>
2022-08-18 21:49:48 -0500

    Implementar prometheus

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from typing import Union
 
 from fastapi import FastAPI
 from pydantic import BaseModel
+from prometheus_fastapi_instrumentator import Instrumentator 
 
 app = FastAPI()
 
@@ -27,3 +28,5 @@ def read_item(item_id: int, q: Union[str, None] = None):
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
+
+Instrumentator().instrument(app).expose(app)
